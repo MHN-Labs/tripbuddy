97b482a460bb6aaf33c281f9c08954b808a18f11
Serge Muhundu <sergemuhundu2@gmail.com>
2024-04-15 12:47:48 +0100

    update

---
+++ src/app/Components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function NavBar() {
+    return (
+        <nav className="h-20">
+            <div>
+                <h1 className=" text-green-600 text-2xl font-bold">TripBuddy</h1>
+            </div>
+        </nav>
+    )
+}
