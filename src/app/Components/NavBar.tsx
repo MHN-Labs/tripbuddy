cb3ba66902c691339f8a4a64b46582e519efd32e
Serge Muhundu <sergemuhundu2@gmail.com>
2024-04-15 15:43:25 +0100

    implement hero section

--- src/app/Components/NavBar.tsx
+++ src/app/Components/NavBar.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React from 'react';
 
 export function NavBar() {
-    return (
-        <nav className="h-20">
-            <div>
-                <h1 className=" text-green-600 text-2xl font-bold">TripBuddy</h1>
-            </div>
-        </nav>
-    )
+	return (
+		<nav className="flex flex-row h-20 shadow-lg px-8 justify-between items-center bg-white fixed w-full top-0 left-0 z-50">
+			<div className=" flex h-full w-40 items-center justify-center content-center align-middle">
+				<h1 className=" text-green-600 text-2xl font-bold align-middle  ">Trip Buddy</h1>
+			</div>
+			<div className="flex flex-row gap-4">
+				<div className="flex justify-center items-center p-4 bg-green-500 rounded-full w-40">
+					<p className="text-white font-bold">Login</p>
+				</div>
+				<div className="flex justify-center items-center p-4 bg-white rounded-full w-40 border-2 border-green-500">
+					<p className=" text-green-500 font-bold">Signup</p>
+				</div>
+			</div>
+		</nav>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,51 @@
-import Image from "next/image";
-import { NavBar } from "./Components/NavBar";
+import Image from 'next/image';
+import { FaGooglePlusG, FaFacebook } from 'react-icons/fa';
+import { NavBar } from './Components/NavBar';
 
 export default function Home() {
-  return (
-    <div>
-      <NavBar />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+	return (
+		<div>
+			<NavBar />
+			<main className="">
+				<div className="hero bg-slate-600 w-full">
+					<Image src={require('../../public/travellers_in_paris.jpg')} alt="" className=" w-full h-screen" />
+				</div>
+				<div className="absolute top-60 bg-white p-6 shadow-lg ml-16 rounded-lg h-auto w-96">
+					<div className="flex flex-col items-center w-full">
+						<h1 className="text-2xl font-bold">Join & Invite.</h1>
+						<h1 className="text-2xl font-bold">Go on a trip together!</h1>
+						<p className="mt-2 text-center">
+							TripBuddy connects thousands of solo travelers across the world.
+						</p>
 
-      </main>
-    </div>
-  );
+						<div className="flex flex-col gap-3 mt-8 items-center w-full">
+							<div className=" flex flex-row  gap-3 justify-center items-center h-10 bg-red-900 rounded-full shadow-md w-full">
+								<FaGooglePlusG size={30} color="#fff" />
+								<p className="text-white">Connect with Google</p>
+							</div>
+
+							<div className=" flex flex-row gap-3 justify-center w-full items-center h-10 bg-blue-600 rounded-full shadow-md">
+								<FaFacebook size={24} color="#fff" />
+								<p className="text-white">Connect with Facebook</p>
+							</div>
+
+							<div>
+								<p>Or</p>
+							</div>
+
+							<div className="flex flex-row justify-between w-full">
+								<div className="flex justify-center items-center p-4 bg-green-500 rounded-full w-40">
+									<p className="text-white font-bold">Register</p>
+								</div>
+
+								<div className="flex justify-center items-center p-4 bg-white rounded-full w-40 border-2 border-green-500">
+									<p className="text-green-500 font-bold">Login</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
 }
